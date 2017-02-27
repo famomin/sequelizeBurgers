b9e2097de76b1634b1087c3e9369da39db9a9221
Faras Momin <famomin@utexas.edu>
2017-02-26 23:14:53 -0600

    made changes for sequelize sync

---
+++ server.js
@@ -0,0 +1,39 @@
+//dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+
+var port = process.env.PORT || 3000;
+
+var app = express();
+
+// Requiring our models for syncing
+var db = require("./models");
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() + "/public"));
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burger_controller.js");
+
+// starts server to begin listening
+db.sequelize.sync({}).then(function() {
+	app.listen(process.env.PORT || 3000, function() {
+		console.log("App listening on PORT " + port);
+	});
+});
